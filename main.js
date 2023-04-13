fca146a65e9b791fd51822e86c6aa631bf975727
mijail1989 <m.surco.developing@gmail.com>
2023-04-13 15:11:26 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,38 @@
+console.log(new Date());
+
+let [seconds, minutes, hours] = [0, 0, 0];
+let timeDisplay = document.getElementById("timeDisplay");
+let timer = null;
+
+function stopwatch() {
+  seconds++;
+  if (seconds == 60) {
+    seconds = 0;
+    minutes++;
+    if (minutes == 60) {
+      minutes = 0;
+      hours++;
+    }
+  }
+
+  let h = hours < 10 ? "0" + hours : hours;
+  let m = minutes < 10 ? "0" + minutes : minutes;
+  let s = seconds < 10 ? "0" + seconds : seconds;
+
+  timeDisplay.innerHTML = h + ":" + m + ":" + s;
+}
+
+function watchStart() {
+  if (timer !== null) {
+    clearInterval(timer);
+  }
+  timer = setInterval(stopwatch, 1000);
+}
+function watchStop() {
+  clearInterval(timer);
+}
+function watchReset() {
+  clearInterval(timer);
+  [seconds, minutes, hours] = [0, 0, 0];
+  timeDisplay.innerHTML = "00:00:00";
+}
